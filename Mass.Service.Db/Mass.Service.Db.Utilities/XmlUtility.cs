a592e7995bd9ba66101a349611945bf85aba9ae0
Mark Rewald <zteam.dev@outlook.com>
2017-08-25 19:02:44 -0700

    Initial Check In

---
+++ Mass.Service.Db/Mass.Service.Db.Utilities/XmlUtility.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Mass.Service.Db.Utilities
+{
+    public class XmlUtility
+    {
+        private XElement elements { get;  set; }
+
+        public string ServerName
+        {
+            get
+            {
+                
+            };
+        }
+
+        public string CatalogName { get; }
+        public string UserName { get; }
+        public string Password { get; }
+        public string ServerType { get; }
+
+        public string SqlStatement { get; }
+        public Dictionary<string, string> SqlParameters { get; }
+
+
+        public string RedCriteria { get; }
+        public string YellowCriteria { get; }
+        public string GreenCriteria { get; }
+
+
+    }
+}
