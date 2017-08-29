127d51388bcfbe85deee9b5a18e4932177dc4d54
mrdenver-pie <zteam.dev@outlook.com>
2017-08-28 21:36:40 -0600

    * Mass.Service.Db.NUnit.csproj: Added NUnit project to replace   MSTest Project
    * Mass.Service.Db.sln:
    * Test.cs:
    * packages.config:
    * Mass.Service.Db.Utilities.csproj:
    * Mass.Service.Db.Utilities.Test.csproj:

---
+++ Mass.Service.Db/Mass.Service.Db.NUnit/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace Mass.Service.Db.NUnit
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
