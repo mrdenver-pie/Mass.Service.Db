b2ce1ec9a65ca372f33d9ac2737e0687e99fa252
jagger <zteam.dev@outlook.com>
2017-09-08 20:27:24 -0600

    Copied tests from Microsoft Tests project

--- Mass.Service.Db/Mass.Service.Db.NUnit/XmlUtilityTests.cs
+++ Mass.Service.Db/Mass.Service.Db.NUnit/XmlUtilityTests.cs
@@ -5,15 +5,16 @@ using Mass.Service.Db.Utilities;
 namespace Mass.Service.Db.NUnit
 {
     [TestFixture()]
-    public class Test
+    public class XmlUtilityTests
     {
         private string _elementsString = "";
 
+        [SetUp]
         public void Init()
         {
             _elementsString = "<pollingRequestData>";
             _elementsString += "      <connectionData>";
-            _elementsString += "          <serverName></serverName>";
+            _elementsString += "          <serverName>sdisssql01</serverName>";
             _elementsString += "          <catalogName>MyUiEmployer</catalogName>";
             _elementsString += "          <userName>MyUiEmployerUser</userName>";
             _elementsString += "          <password>MyUiEmployerUser</password>";
@@ -61,6 +62,78 @@ namespace Mass.Service.Db.NUnit
                 Assert.AreEqual("ERROR 1 - Invalid Server Name", e.Message);
             }
         }
+        
+        [Test()]
+        public void GetCatalogNameTest()
+        {
+            try
+            {
+                XmlUtility xmlUtility = new XmlUtility(_elementsString);
+                string catalogNameToTest = "MyUiEmployer";
+                string catalogNameTested = "";
 
+                catalogNameTested = xmlUtility.CatalogName;
+                Assert.AreEqual(catalogNameToTest, catalogNameTested);
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual("ERROR 2 - Invalid Catalog Name", e.Message);
+            }
+
+        }
+        
+        [Test()]
+        public void GetUserNameTest()
+        {
+            try
+            {
+                XmlUtility xmlUtility = new XmlUtility(_elementsString);
+                string userNameToTest = "MyUiEmployerUser";
+                string userNameTested = "";
+
+                userNameTested = xmlUtility.UserName;
+                Assert.AreEqual(userNameToTest, userNameTested);
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual("ERROR 3 - Invalid User Name", e.Message);
+            }
+        }
+        
+        [Test()]
+        public void GetPasswordTest()
+        {
+            try
+            {
+                XmlUtility xmlUtility = new XmlUtility(_elementsString);
+                string passwordToTest = "MyUiEmployerUser";
+                string passwordTested = "";
+
+                passwordTested = xmlUtility.Password;
+                Assert.AreEqual(passwordToTest, passwordTested);
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual("ERROR 4 - Invalid Password", e.Message);
+            }
+        }   
+        
+        [Test()]
+        public void GetServerTypeTest()
+        {
+            try
+            {
+                XmlUtility xmlUtility = new XmlUtility(_elementsString);
+                string serverTypeToTest = "SQL Server";
+                string serverTypeTested = "";
+
+                serverTypeTested = xmlUtility.ServerType;
+                Assert.AreEqual(serverTypeToTest, serverTypeTested);
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual("ERROR 5 - Invalid Server Type", e.Message);
+            }
+        }        
     }
 }
